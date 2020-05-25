d9a7332113a74070dfdfd3e3bd1c3040a49a1f8c
intersim <emily.intersimone@gmail.com>
2020-05-24 22:33:24 -0400

    break down into smaller funcs

--- getUnreleased.go
+++ getUnreleased.go
@@ -1,55 +1,82 @@
-package chchchanges
+package unreleased
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 // import "github.com/go-git/go-git/v5"
 
-func getUnreleased() {
-	// get current directory
-	// get current branch
-
-	// get repos from config file
-	// for each repo:
-	// cd to it
-	// get unreleased changes
-	// log out changes info
-
-	// cd back to current directory
-	// change back to current branch
-}
-
 func getCurrentDirectory() {
 	// https://gist.github.com/arxdsilva/4f73d6b89c9eac93d4ac887521121120
+	cmd := exec.Command("pwd")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	var person struct {
+		Name string
+		Age  int
+	}
+	if err := json.NewDecoder(stdout).Decode(&person); err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s is %d years old\n", person.Name, person.Age)
 }
 
-func getCurrentBranch() {
+func getCurrentBranch() string {
 	// https://golang.org/pkg/os/exec/
+	// git rev-parse --abbrev-ref HEAD
+	return "master"
 }
 
-func getReposFromFile() {
-	// read from chchchanges.config file (filename?)
+func getReposFromFile() []string {
+	// read from unreleased.config file (filename?)
 	// get array of paths to look at, or array of repo names to look for?
+	return []string{"/path/to/my_repo"}
 }
 
-func getUnreleasedChanges() {
-	// function get_unreleased_git_helper {
-	// 	(git checkout master && git pull origin master) > /dev/null 2>&1
-	// 	repo_name=$1
-	// 	last_tag="$(git describe --tag --abbrev=0)"
+func getRepoName() string {
+	// basename `git rev-parse --show-toplevel`
+	return "my_repo"
+}
+
+func updateMaster() {
+	// (git checkout master && git pull origin master) > /dev/null 2>&1
+}
+
+func getLastTag() string {
+	// last_tag="$(git describe --tag --abbrev=0)"
+	return "my-last-tag"
+}
+
+func getNumCommits(repoName string) int {
 	// 	num_commits="$(git rev-list $last_tag..HEAD --count)"
-	// 	get_unreleased_log_helper $repo_name $num_commits $last_tag
-	// }
+	return 1
 }
 
-func logUnreleasedChanges() {
-	// function get_unreleased_log_helper {
-	// 	repo_name=$1
-	// 	num_commits=$2
-	// 	last_tag=$3
-	// 	if [ "$num_commits" -gt "1" ]; then
-	// 		echo ":white_check_mark: $repo_name: There are $num_commits unreleased commits! The last release was: $last_tag"
-	// 	elif [ "$num_commits" == "1" ]; then
-	// 		echo ":white_check_mark: $repo_name: There is $num_commits unreleased commit! The last release was: $last_tag"
-	// 	else
-	// 		echo ":x: $repo_name: No changes to release. The last release was: $last_tag"
-	// 	fi
-	// }
+func logUnreleasedChanges(lastTag string, repoName string, numCommits int) {
+	var numCommitsMsg string = ""
+	var emoji string = ""
+
+	if numCommits > 1 {
+		numCommitsMsg = fmt.Sprintf("There are %d unreleased commits!", numCommits)
+		emoji = ":white_check_mark:"
+	} else if numCommits == 1 {
+		numCommitsMsg = fmt.Sprintf("There is %d unreleased commit!", numCommits)
+		emoji = ":white_check_mark:"
+	} else {
+		numCommitsMsg = fmt.Sprintf("No changes to release.")
+		emoji = ":x:"
+	}
+
+	fmt.Printf("%s %s The last release was: %s", emoji, numCommitsMsg, lastTag)
 }
--- index.go
+++ index.go
@@ -1,7 +1,15 @@
-// https://genius.com/David-bowie-changes-lyrics
-
-package chchchanges
+package unreleased
 
 func main() {
+	// get current directory
+	// get current branch
 
+	// get repos from config file
+	// for each repo:
+	// cd to it
+	// get unreleased changes
+	// log out changes info
+
+	// cd back to current directory
+	// change back to current branch
 }
